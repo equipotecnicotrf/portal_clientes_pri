37366fe144e23a968cde9f91200abdd0511f0bae
equipotecnicotrf <equipotecnicotransforma@gmail.com>
2023-10-07 20:28:46 -0500

    gabril 7-10 8:30

--- src/main/java/com/portalClientesPrimadera/model/TypeOrderEntity.java
+++ src/main/java/com/portalClientesPrimadera/model/TypeOrderEntity.java
@@ -15,11 +15,11 @@ public class TypeOrderEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long CP_type_order_id;
 
-    @Column(name = "CP_type_order_description")
-    private String CP_type_order_description;
-
     @Column(name = "CP_type_order_meaning")
     private String CP_type_order_meaning;
+
+    @Column(name = "CP_type_order_description")
+    private String CP_type_order_description;
 
     @Column(name = "CP_type_order_status")
     private String CP_type_order_status;
