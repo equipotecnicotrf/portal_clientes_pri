8abc501c4b9b9709829cd0077b2c3de87d76c737
Deivy Nicolas Castiblanco Infante <dcastiblanco@oadmin.com>
2023-10-09 15:18:21 -0500

    Carga archivos entity price-IVA-Availability

---
+++ src/main/java/com/portalClientesPrimadera/model/AvailabilityEntity.java
@@ -0,0 +1,30 @@
+package com.portalClientesPrimadera.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "Availability", uniqueConstraints = @UniqueConstraint(columnNames = "inventory_item_id"))
+public class AvailabilityEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inventory_item_i;
+
+    @Column(name = "organization_id")
+    private Long organization_id;
+
+    @Column(name = "organization_code")
+    private String organization_code;
+
+    @Column(name = "Quantity_units")
+    private Long Quantity_units;
+
+    @Column(name = "Quantity_packages")
+    private Long Quantity_packages;
+
+    
+}
---
+++ src/main/java/com/portalClientesPrimadera/model/PricesEntity.java
@@ -0,0 +1,35 @@
+package com.portalClientesPrimadera.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "Prices", uniqueConstraints = @UniqueConstraint(columnNames = "Prices_id"))
+public class PricesEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Prices_id;
+
+    @Column(name = "Cust_account_id")
+    private Long Cust_account_id;
+
+    @Column(name = "Inventory_item_id")
+    private Long Inventory_item_id;
+
+    @Column(name = "Organization_id")
+    private Long Organization_id;
+
+    @Column(name = "currency_code")
+    private String currency_code;
+
+    @Column(name = "unit_price")
+    private Long unit_price;
+
+    @Column(name = "CP_IVA_id")
+    private String cust_name;
+
+}
