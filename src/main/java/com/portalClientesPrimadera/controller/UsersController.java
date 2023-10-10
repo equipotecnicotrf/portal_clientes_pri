e267251d0394e59c455421163b8a7657a551c7fc
Juan Diego Robles Gonzalez <jrobles@oadmin.com>
2023-10-10 17:01:22 -0500

    add contraseña

--- src/main/java/com/portalClientesPrimadera/controller/UsersController.java
+++ src/main/java/com/portalClientesPrimadera/controller/UsersController.java
@@ -79,13 +79,4 @@ public class UsersController {
     }
 
 
-    @PutMapping("/Users/{id}/update-password")
-    public ResponseEntity actualizarPassword(@PathVariable Long id, @RequestBody String newPassword) {
-    UsersEntity users = usersRepository.findById(id)
-    .orElseThrow(() -> new ResourceNotFoundException("El usuario con este ID no existe : " + id));
-    String encodeNewPassword = Base64.getEncoder().encodeToString(newPassword.getBytes());
-    users.setCP_Password(encodeNewPassword);
-    UsersEntity userActualizado = usersRepository.save(users);
-    return ResponseEntity.ok(userActualizado);
-}
 }
