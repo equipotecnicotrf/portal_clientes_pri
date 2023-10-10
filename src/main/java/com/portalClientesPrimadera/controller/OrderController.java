0d82dcc2ace5de14c9ff5d93e96b106c0ae24bb4
Juan Diego Robles Gonzalez <jrobles@oadmin.com>
2023-10-10 09:57:03 -0500

    10-10-2023 Controllers

---
+++ src/main/java/com/portalClientesPrimadera/controller/OrderController.java
@@ -0,0 +1,44 @@
+package com.portalClientesPrimadera.controller;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.portalClientesPrimadera.exception.ResourceNotFoundException;
+import com.portalClientesPrimadera.model.OrderEntity;
+import com.portalClientesPrimadera.repository.OrderRepository;
+
+@CrossOrigin(origins =  "http://localhost:5173/")
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    @Autowired
+    OrderRepository orderRepository; 
+    
+    @GetMapping ("/Order")
+    public List <OrderEntity> listOrders(){
+        return orderRepository.findAll();
+    }
+
+    @PostMapping ("/Order")
+    public OrderEntity saveOrder (@RequestBody OrderEntity Order){
+        return orderRepository.save(Order);
+    }
+
+    @GetMapping ("/Order/{id}")
+    public ResponseEntity <OrderEntity> getOrderByid(@PathVariable Long id){
+        OrderEntity order = orderRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("La orden no se encuentra con el id "+ id ));
+        return ResponseEntity.ok(order);
+    }
+}
