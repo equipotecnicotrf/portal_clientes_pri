2e0b71236f8344c1b1b68826fc8825243b68c92c
Gabrielgo25 <josegabrielgonzalezmartinez@gmail.com>
2023-12-03 17:30:32 -0500

    Gabriel 3-12-23 5:30 pm

--- src/main/java/com/portalClientesPrimadera/controller/AccessController.java
+++ src/main/java/com/portalClientesPrimadera/controller/AccessController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -32,6 +33,11 @@ public class AccessController {
         AccessEntity access = accessRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("El acceso no existe con el CP_Access_id" + id));
         return ResponseEntity.ok(access);
+    }
+
+    @GetMapping("/AccessAndContext")
+    public List<ArrayList> getAllAccessandContext(@RequestParam(name = "cp_rol_id") Long CP_rol_id) {
+        return accessRepository.findByAccessandContext(CP_rol_id);
     }
 
 }
